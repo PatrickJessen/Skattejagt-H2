370652407a71bb983df9849399458db3878fa29b
patr4586 <patr4586@ZBC-E-RO-23234>
2021-11-23 10:08:04 +0100

    commit

--- App.js
+++ App.js
@@ -1,5 +1,7 @@
 let xPos = 0;
 let yPos = 0;
+let clickCount = 0;
+let distanceHelper = 90;
 
 window.onload = Start()
 {
@@ -12,28 +14,67 @@ function Start()
     document.getElementById("tresure").style.position = "absolute";
     document.getElementById("tresure").style.color = "red";
     document.getElementById("tresure").style.fontSize = "xx-large";
-    //document.getElementById("tresure").style.visibility = "hidden";
+    document.getElementById("tresure").style.visibility = "hidden";
     document.getElementById("tresure").style.left = xPos + "px";
     document.getElementById("tresure").style.top = yPos + "px";
     document.getElementById("tresure").style.cursor = "default";
+    document.getElementById("checker").innerHTML = "";
+    clickCount = 0;
 }
 
-function InRange(low, high, sum)
+function InRangeX(low, high, sum)
 {
-    return (low <= sum && sum <= high)
+    return ((sum - high) * (sum - low) <= 0);
 }
 
 function OnClick()
 {
-    if (InRange(xPos - 50, xPos + 50, event.clientX) || InRange(xPos + 50, xPos - 50, event.clientX) && InRange(yPos - 50, yPos + 50, event.clientY) || InRange(yPos + 50, yPos - 50, event.clientY))
+    if (!IsGameOver())
     {
-        console.log("found");
-        document.getElementById("tresure").style.visibility = "visible";
+        const yClick = event.clientY - 50;
+        if (InRangeX(xPos + distanceHelper, xPos - distanceHelper, event.clientX) && InRangeX(yPos + distanceHelper, yPos - distanceHelper, yClick))
+        {
+            document.getElementById("tresure").style.visibility = "visible";
+            document.getElementById("checker").innerHTML = "YOU WON!";
+            setTimeout(Start, 15000);
+        }
+        Helper(event.clientX, yClick);
+        clickCount++;
     }
-    //if (event.clientX <= xPos - 10 && event.clientX >= xPos + 10 && event.clientY <= yPos - 10 && event.clientY >= yPos + 10 )
-    //{
-    //    console.log("found");
-    //    document.getElementById("tresure").style.visibility = "visible";
-    //}
+    else
+    {
+        document.getElementById("checker").innerHTML = "YOU LOST!";
+        document.getElementById("tresure").style.visibility = "visible";
+        setTimeout(Start, 15000);
+    }
+}
+
+function IsGameOver()
+{
+    if (clickCount >= 4)
+    {
+        return true;
+    }
+    return false;
+}
+
+function Helper(clickX, clickY)
+{
+    if (clickX > xPos && clickY > yPos)
+    {
+        document.getElementById("checker").innerHTML = "More to the left and up!";
+    }
+    else if (clickX < xPos && clickY < yPos)
+    {
+        document.getElementById("checker").innerHTML = "More to the right and down!";
+    }
+    else if (clickX > xPos && clickY < yPos)
+    {
+        document.getElementById("checker").innerHTML = "More to the left and down!";
+    }
+    else if (clickX < xPos && clickY > yPos)
+    {
+        document.getElementById("checker").innerHTML = "More to the right and up!";
+    }
 }
 
